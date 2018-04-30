97bea24faa02954c20c17959e54c55bd0e1f8e37
Eddie Yepez <eddieyepez@yahoo.com>
2018-04-30 01:25:03 -0400

    Final app

--- app.js
+++ app.js
@@ -33,36 +33,83 @@ fetch(url).then(res => {
 
 let build = () => {
 
-		console.log(clientsInfo);
+	//console.log(clientsInfo);
 
-		for( let i = 0; i < clientsInfo.length ; i++){
+	for( let i = 0; i < clientsInfo.length ; i++){
 
-			if( clientsInfo[i].stream ){
-				document.querySelector('#cardsContainer').innerHTML += ""+
-				"<a href="+clientsInfo[i].url+" target='_blank'>"+
-				"<div class='card'>"+
-				"<div class='card-header' style='background-image: url("+ clientsInfo[i].banner +")'>"+
-				"<div><h2 class='name'>"+clientsInfo[i].name+"</h2></div>"+
-				"<div><img class='logo' src='"+clientsInfo[i].logo+"'></div>"+
-				"</div>"+
-				"<div class='stream'><h4>Streaming: </h4></div>"+
-				"<div class='info'>"+clientsInfo[i].status+"</div>"+
-				"</div></a>"
-			}else{
-				document.querySelector('#cardsContainer').innerHTML += ""+
-				"<div class='card offline'>"+
-				"<div class='card-header'>"+
-				"<div class='card-header-bg' style='background-image: url("+ clientsInfo[i].banner +")'></div>"+
-				"<div><h2 class='name'>"+clientsInfo[i].name+"</h2></div>"+
-				"<div><img class='logo' src='"+clientsInfo[i].logo+"'></div>"+
-				"</div>"+
-				"<div><h4>Offline</h4></div>"+
-				"</div>"
-			}
+		if( clientsInfo[i].stream ){
+			document.querySelector('#cardsContainer').innerHTML += ""+
+			"<a href="+clientsInfo[i].url+" target='_blank'>"+
+			"<div class='card online show'>"+
+			"<div class='card-header' style='background-image: url("+ clientsInfo[i].banner +")'>"+
+			"<div><h2 class='name'>"+clientsInfo[i].name+"</h2></div>"+
+			"<div><img class='logo' src='"+clientsInfo[i].logo+"'></div>"+
+			"</div>"+
+			"<div class='stream'><h4>Streaming: </h4></div>"+
+			"<div class='info'>"+clientsInfo[i].status+"</div>"+
+			"</div></a>"
+		}else{
+			document.querySelector('#cardsContainer').innerHTML += ""+
+			"<div class='card offline show'>"+
+			"<div class='card-header'>"+
+			"<div class='card-header-bg' style='background-image: url("+ clientsInfo[i].banner +")'></div>"+
+			"<div><h2 class='name'>"+clientsInfo[i].name+"</h2></div>"+
+			"<div><img class='logo' src='"+clientsInfo[i].logo+"'></div>"+
+			"</div>"+
+			"<div><h4>Offline</h4></div>"+
+			"</div>"
+		}
+	}
 
 
-		}	
+	document.querySelector('#btn-offline').addEventListener("click", () => {
 
-	
+		var offlineCards = document.getElementsByClassName('offline');
+	    for (i = 0; i < offlineCards.length; i++) {
+	    	offlineCards[i].classList.add('show');
+	    }
+
+		var onlineCards = document.getElementsByClassName('online');
+	    for (i = 0; i < onlineCards.length; i++) {
+	        onlineCards[i].classList.add('hide');
+	        onlineCards[i].classList.remove('show');
+	    }
+
+	});
+
+	document.querySelector('#btn-online').addEventListener("click", () => {
+
+		var onlineCards = document.getElementsByClassName('online');
+	    for (i = 0; i < onlineCards.length; i++) {
+	        onlineCards[i].classList.add('show');
+	    }
+
+	    var offlineCards = document.getElementsByClassName('offline');
+	    for (i = 0; i < offlineCards.length; i++) {
+	    	offlineCards[i].classList.add('hide');
+	        offlineCards[i].classList.remove('show');
+	    }
+
+	});
+
+	document.querySelector('#btn-all').addEventListener("click", () => {
+
+		var onlineCards = document.getElementsByClassName('online');
+	    for (i = 0; i < onlineCards.length; i++) {
+	        onlineCards[i].classList.add('show');
+	        onlineCards[i].classList.remove('hide');
+	    }
+
+	    var offlineCards = document.getElementsByClassName('offline');
+	    for (i = 0; i < offlineCards.length; i++) {
+	        offlineCards[i].classList.add('show');
+	        offlineCards[i].classList.remove('hide');
+	    }
+
+	});
+
+
 }
+
+
 
